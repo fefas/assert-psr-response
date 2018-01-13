8073d5fb4045223770f9c240a620f93e0a5dbaa3
Felipe Martins <me@fefas.net>
2018-01-13 23:57:13 +0100

    Remove phpunit from JsonBody assertion (resolves #3)

--- src/Assertions/JsonBodyAssertion.php
+++ src/Assertions/JsonBodyAssertion.php
@@ -3,33 +3,33 @@
 namespace Fefas\AssertPsrResponse\Assertions;
 
 use Psr\Http\Message\ResponseInterface as Response;
+use Fefas\Jsoncoder\Json;
 
 class JsonBodyAssertion implements Assertion
 {
     private const FAILED_MESSAGE_TEMPLATE =
         'Failed asserting response json body \'%s\' to the expected \'%s\'';
 
-    private $isFailed;
+    private $isFailed = false;
     private $failedMessage;
 
     public function __construct(string $expectedJsonBody, Response $responseToAssert)
     {
         $jsonBodyToAssert = $responseToAssert->getBody()->getContents();
 
-        try {
-            assertJsonStringEqualsJsonString($expectedJsonBody, $jsonBodyToAssert);
+        $expectedJson = Json::createFromString($expectedJsonBody);
+        $jsonToAssert = Json::createFromString($jsonBodyToAssert);
 
-            $this->isFailed = false;
-        } catch (\Exception $phpunitException) {
-        //    $jsonDiff = $phpunitException->getComparisonFailure()->getDiff();
-
-            $this->isFailed = true;
-
-            $this->failedMessage = $this->buildFailedMessage(
-                $expectedJsonBody,
-                $jsonBodyToAssert
-            );
+        if ($jsonToAssert->isEqualTo($expectedJson)) {
+            return;
         }
+
+        $this->isFailed = true;
+
+        $this->failedMessage = $this->buildFailedMessage(
+            $expectedJsonBody,
+            $jsonBodyToAssert
+        );
     }
 
     public function isFailed(): bool
